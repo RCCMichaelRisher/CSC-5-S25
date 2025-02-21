d59b6bc721abeb54928dcc2570089160a22d8cfd
RCC Michael Risher <michael.risher@rccd.edu>
2025-02-20 17:33:05 -0800

    Hello World Program

---
+++ helloWorld/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * Author: Michael Risher
+ * Purpose: My first program
+ */
+
+//System Libraries
+#include <iostream>  //Input Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    cout << "Hello World!" << endl;
+    //Clean up memory and files
+    
+    //Exit the Program
+    return 0;
+}
